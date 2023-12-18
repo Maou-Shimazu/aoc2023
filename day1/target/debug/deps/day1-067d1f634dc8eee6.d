44932fe9853e2b2297b3110704fb91dfbe319dbf
Maou Shimazu <eldaddanladi@gmail.com>
2023-12-18 03:35:09 -0600

    Day 1 question

---
+++ day1/target/debug/deps/day1-067d1f634dc8eee6.d
@@ -0,0 +1,5 @@
+/home/maou/aoc2023/day1/target/debug/deps/libday1-067d1f634dc8eee6.rmeta: src/main.rs
+
+/home/maou/aoc2023/day1/target/debug/deps/day1-067d1f634dc8eee6.d: src/main.rs
+
+src/main.rs:
---
+++ day1/target/debug/deps/day1-16a21c4b636640eb.d
@@ -0,0 +1,5 @@
+/home/maou/aoc2023/day1/target/debug/deps/libday1-16a21c4b636640eb.rmeta: src/main.rs
+
+/home/maou/aoc2023/day1/target/debug/deps/day1-16a21c4b636640eb.d: src/main.rs
+
+src/main.rs:
